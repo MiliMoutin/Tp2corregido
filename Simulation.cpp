3acd70de612e72a3d4f28892660356dfd949ac23
Francisco Daniel Ledesma <fledesma@itba.edu.ar>
2019-03-26 10:38:50 -0300

    circulo robot

--- Simulation.cpp
+++ Simulation.cpp
@@ -1,5 +1,6 @@
 #include "Simulation.h"
 
+
 PSimulation createSimulation(int n, int m, int nRobots) {
 	PSimulation toret = (PSimulation)malloc(sizeof(Simulation));
 	if (toret != NULL) {
@@ -11,3 +12,20 @@ PSimulation createSimulation(int n, int m, int nRobots) {
 	}
 	else return NULL;
 }
+
+
+
+
+/*
+
+void Create_dirty_floor(int col, int fil, float size_floor); //coloca todos los pisos sucios. recibe columna y fila.
+al_flip_display(); //actualiza lo que ves en pantalla
+al_rest(1.0); //delay 1.0
+
+void Clean_floor(int x, int y, float); //
+void Set_robot(float x, float y, float angle, allegro_t * allegro_p, float size_floor);
+
+
+
+
+*/
